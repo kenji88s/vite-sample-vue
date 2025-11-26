dbdd71db7f40ad66356830c969a91e29950c76f9
kenji88s <mul88ti.ch63i@gmail.com>
2025-11-26 09:55:32 +0900

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="wrapper">
+    <h1>Vue + Vite 完全版 SPA</h1>
+    <p>これは Vite + Vue 公式構成に準拠した完全版 SPA です。</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+.wrapper {
+  font-family: sans-serif;
+  padding: 2rem;
+}
+</style>
